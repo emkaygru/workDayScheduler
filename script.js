76fb7f5acba2fbabd3d1f54ce22d1d4a87375e9b
Emily Groenendal <emilylauder@gmail.com>
2020-10-11 22:43:53 -0400

    'updated html and js'

--- script.js
+++ script.js
@@ -2,164 +2,126 @@
 let m = moment();
 // current time/hour
 let currentTime = m.format('h:mma');
-// save button variable
-let buttons = document.getElementsByClassName('saveBtn');
+
 // empty array for the schedule blocks
 let tempStorage = [];
 let apptText = '';
 let apptTime = '';
 
 let timeBlockContainer = document.getElementsByClassName('time-block');
-// previous Appts are previous appts saved on local Storage
-let previousAppts;
-// saved Appts are the appts saved during the current session  - will become previous one refreshed
-let savedAppts;
+
+var storedAppointments;
+var returnedAppointments;
 
 // variables for the time blocks on the schedule 
-var nine = m.hour(9);
-$("#nine").html('9AM');
+var nine = $("#nine").html('9AM');
+var ten = $('#ten').html('10AM');
+var eleven = $('#eleven').html('11AM');
+var twelve = $('#twelve').html('12PM');
+var thirteen = $('#thirteen').html('1PM');
+var thirteen = $('#fourteen').html('2PM');
+var thirteen = $('#fifteen').html('3PM');
+var thirteen = $('#sixteen').html('4PM');
+var thirteen = $('#seventeen').html('5PM');
 
-var ten = m.hour(10);
-$('#ten').text('10AM');
-
-var eleven = m.hour(11);
-$('#eleven').html('11AM');
-
-var twelve = m.hour(12);
-$('#twelve').html('12PM');
-
-var thirteen = m.hour(13);
-$('#thirteen').html('1PM');
-
-var thirteen = m.hour(14);
-$('#fourteen').html('2PM');
-
-var thirteen = m.hour(15);
-$('#fifteen').html('3PM');
-var thirteen = m.hour(16);
-$('#sixteen').html('4PM');
-
-var thirteen = m.hour(17);
-$('#seventeen').html('5PM');
 // display the time at the top of the schedule - time and date
 let date = m.format("dddd MMM Do");
 
-// call the view Schedule function on load to view the current schedule
-// viewSchedule();
 $(document).ready(function () {
     $('#currentDay').html(`${date}`);
     $('#currentTime').html(`${currentTime}`);
 
-
+    // console.log(localStorage);
 
     function viewSchedule() {
 
-        savedAppts = JSON.parse(localStorage.getItem('scheduleBlocks'));
+        storedAppointments = JSON.parse(localStorage.getItem('scheduleBlocks'));
 
-        if (savedAppts !== null) {
-            for (i = 0; i < savedAppts.length; i++) {
-                previousAppts = savedAppts[i];
-                details = previousAppts.details;
-                timeIndex = previousAppts.time;
+        if (storedAppointments !== null) {
+            for (i = 0; i < storedAppointments.length; i++) {
+                returnedAppointments = storedAppointments[i];
+                details = returnedAppointments.details;
+                timeIndex = returnedAppointments.time;
 
-                // if (details !== null) {
-                //     $("#" + timeIndex).children('div').children('div').children('textarea').val(details);
-                // }
+                if (details !== null) {
+                    $("#" + timeIndex).children('div').children('div').children('textarea').val(details);
+                }
+            }
+
+        }
+
+
+        function changeColor() {
+            for (i = 9; i <= 17; i++) {
+                let scheduleTime = moment().hour();
+                scheduleTime = i;
+
+                if (currentTime === i) {
+                    $('.inputText' + i).addClass('present');
+
+                } else if (currentTime > i) {
+                    $('.inputText' + i).addClass('past');
+
+                } else {
+                    $('.inputText' + i).addClass('future');
+
+                }
             }
         }
 
-    };
+        changeColor();
+        // $(document.body).click(function () {
 
+        //     $("div.inputText").each(function (i) {
+        //         if (hour === currentTime) {
+        //             $(this).addClass('present').removeClass('future').removeClass('past');
+        //         } else if (hour > currentTime) {
+        //             $(this).addClass('past');
+        //         } else {
+        //             $(this).addClass('future').removeClass('past').removeClass('present');
 
-    viewSchedule();
+        //         }
+        //     });
+        // });
 
-    for (i = 0; i <= 23; i++) {
-        let hour = document.getElementsByClassName('hour');
-
-        if (hour === currentTime) {
-            $('div.inputText').addClass('present').removeClass('future').removeClass('past');
-
-        } else if (hour < currentTime) {
-            $('div.inputText').addClass('past');
-
-        } else {
-            $('div.inputText').addClass('future').removeClass('past').removeClass('present');
-
-        }
     }
-    // $(document.body).click(function () {
+    viewSchedule();
+});
 
-    //     $("div.inputText").each(function (i) {
-    //         if (hour === currentTime) {
-    //             $(this).addClass('present').removeClass('future').removeClass('past');
-    //         } else if (hour > currentTime) {
-    //             $(this).addClass('past');
-    //         } else {
-    //             $(this).addClass('future').removeClass('past').removeClass('present');
 
-    //         }
-    //     });
-    // });
-
-})
 // add a class for past, present and future events/appointments for the day
+let time = document.getElementsByClassName('display-time');
+let data = document.getElementsByClassName('inputText');
 
+// localStorage.setItem("scheduleBlocks " + time, data);
 $('.saveBtn').click(function () {
     apptText = $(this).parent('div').children('div').children('textarea').val();
     apptTime = $(this).parent('div').parent().attr('id');
-    appt = {
+    let appointment = {
         time: apptTime,
         details: apptText
-    }
+    };
 
     tempStorage = JSON.parse(localStorage.getItem('scheduleBlocks'));
-
+    localStorage.setItem('scheduleBlocks', JSON.stringify([{
+        time: apptTime,
+        details: apptText
+    }]));
     if (tempStorage === null) {
         localStorage.setItem('scheduleBlocks', JSON.stringify([{
             time: apptTime,
             details: apptText
         }]));
     } else {
-        tempStorage.push(appt);
+        tempStorage.push(appointment);
         localStorage.setItem('scheduleBlocks', JSON.stringify(tempStorage));
     }
     $(this).parent('div').children('div').children('textarea').replaceWith($('<textarea>' + apptText + '</textarea>').addClass('textAreaInput'));
-})
 
 
+});
 
 
-
-
-// grab items from local storage 
-// var storedScheduleItems = JSON.parse(localStorage.getItem("scheduleBlocks"));
-// on click save the schedule to localStorage and run setSchedule
-// $("buttons").on("click", setSchedule());
-
-// // set schedule function to add text to the schedule blocks, add them to local Storage and append them to the text box.
-// function setSchedule() {
-//     let timeBlocks = $(".schedule").val();
-
-//     if (timeBlocks !== "") {
-//         for (var i = 0; i < scheduleBlocks.length; i++) {
-
-
-//             scheduleBlocks.push(timeBlocks);
-
-//             localStorage.setItem('scheduleBlocks', JSON.stringify(scheduleBlocks));
-//             $(".schedule-text").text(scheduleBlocks);
-//             $(".schedule-text").html(scheduleBlocks[i]);
-//         }
-//     } else return;
-
-//     let textInput = $('<p>');
-//     textInput.text(timeBlocks);
-//     $('.schedule-text').append(textInput);
-
-
-
-//     event.preventDefault();
-// }
 
 
 
